1ff73ac0f2a9bf1c7fa0d9a656d334891da46bc7
D Sofer <d@sofer.com>
2011-01-07 14:59:36 +0000

    project started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icify::Application.config.secret_token = '3554c5f8dbc6788e7dfe1ba3185aa226a762abed2276e402602a1d99cfedb336a8c533793f890497d78af8e465756196827bcac247faec2639723fa285d3d7a3'
